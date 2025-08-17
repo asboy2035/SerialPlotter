b9f86e6904b7e30590df493f491a48bce7400bb0
ash <ash@asboy2035.com>
2025-08-17 11:35:15 +0200

    Initial Commit

---
+++ SerialPlotter/SerialPlotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerialPlotterApp.swift
+//  SerialPlotter
+//
+//  Created by ash on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SerialPlotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
